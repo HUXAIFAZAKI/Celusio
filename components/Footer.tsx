467a550a026a91ef9296d68baef4f4ea463cc899
HUXAIFAZAKI <huzaifazaki92@gmail.com>
2025-02-23 23:26:01 +0500

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Play } from "lucide-react";
+// import { Play } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Input } from "./ui/input";
